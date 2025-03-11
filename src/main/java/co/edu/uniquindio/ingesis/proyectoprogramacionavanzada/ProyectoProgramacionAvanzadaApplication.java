a3bc938d1478ec2bd9414b45a5a1ace467287f26
murde <juand.lopezh@uqvirtual.edu.co>
2025-03-11 11:50:25 -0500

    Se crea el proyecto de progamacion avanzada

---
+++ src/main/java/co/edu/uniquindio/ingesis/proyectoprogramacionavanzada/ProyectoProgramacionAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.ingesis.proyectoprogramacionavanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoProgramacionAvanzadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoProgramacionAvanzadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniquindio/ingesis/proyectoprogramacionavanzada/ProyectoProgramacionAvanzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.ingesis.proyectoprogramacionavanzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoProgramacionAvanzadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
